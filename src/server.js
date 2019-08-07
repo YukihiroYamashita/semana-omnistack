0eb642953c5d706955f4221c42948813df4b2ee1
elcachorroloko <raitoyagami2013@live.com>
2019-08-06 20:26:30 -0400

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
+
+const mongoose = require('mongoose');
 
 const server = express();
 
+mongoose.connect('mongodb+srv://yukihiro:yamashita@cluster0-hwtgk.mongodb.net/omnistack?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+server.use(cors());
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
